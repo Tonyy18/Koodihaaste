d99bb24fda6fb70d0e7520b9248b5c94f6dbda1c
Toni Isotalo <toni.isotalo@hotmail.com>
2022-10-21 21:05:16 +0300

    Windows os support for path parsing

--- modules/path.js
+++ modules/path.js
@@ -2,6 +2,15 @@
 //Returns the absolute path to the root directory
 //Easier to get its path from submodules
 let path = __dirname.split("/");
+let os = "linux"
+if(path.length < 2) {
+    path = __dirname.split("\\");
+    os = "windows"
+}
 path.pop()
-path = path.join("/")
+if(os == "windows") {
+    path = path.join("\\")
+} else {
+    path = path.join("/")
+}
 module.exports = path
